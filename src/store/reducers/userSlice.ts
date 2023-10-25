4ff0ad704571fb1f9bd16f5e2c3e3e8003eac750
Ashot08 <albertguzeev@yandex.ru>
2023-10-25 16:28:52 +0700

    init, add typescript, user reducer

---
+++ src/store/reducers/userSlice.ts
@@ -0,0 +1,31 @@
+import {createSlice} from "@reduxjs/toolkit";
+import {RootState} from "../store.ts";
+
+export interface UserState {
+  isLogin: boolean,
+  username: string,
+  name: string,
+  email: string,
+  password: string,
+}
+
+const initialState :UserState = {
+  isLogin: false,
+  username: '',
+  name: '',
+  email: '',
+  password: '',
+};
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+
+  }
+});
+
+export const selectUserLogin = (state: RootState) => state.user.username;
+export const selectUserName = (state: RootState) => state.user.name;
+
+export default userSlice.reducer;
