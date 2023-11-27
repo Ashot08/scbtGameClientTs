35d04339cdae0b10fa8b3f04dd6c36fe3f72652c
Ashot08 <albertguzeev@yandex.ru>
2023-11-27 17:28:55 +0700

    add results graph

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -3,13 +3,10 @@ import './game.css';
 import {useAppDispatch, useAppSelector} from "../../hooks.ts";
 import {selectUserIsLogin, selectUserLogin, selectUserName} from "../../store/reducers/userSlice.ts";
 import Token from "../../utils/Token.ts";
-
-// import RouletteMobile from "../RouletteMobile/RouletteMobile.jsx";
-// import {mobileCheck} from "../../utils/mobileCheck.js";
 import Roulette from "../Roulette/Roulette.jsx";
 import BasicCard from "../Card/BasicCard.tsx";
 import List from "@mui/material/List";
-import {ListItem, ListItemButton, ListItemIcon, ListItemText, TextField} from "@mui/material";
+import {ListItem, ListItemButton, ListItemIcon, ListItemText, Tab, Tabs, TextField} from "@mui/material";
 import {GameQR} from "../GameQR/GameQR.tsx";
 import {hidePopup, showPopup} from "../../store/reducers/popupSlice.ts";
 import FaceIcon from '@mui/icons-material/Face';
@@ -20,12 +17,24 @@ import CasinoIcon from '@mui/icons-material/Casino';
 import {selectPlayerName, selectResult} from "../../store/reducers/rouletteSlice.ts";
 import {hide, selectIsActive, selectTimerOn, show} from "../../store/reducers/quizSlice.ts";
 import {Quiz} from "../Quiz/Quiz.tsx";
-import {getAnswersResults, getLastRoll, getLastRollMainAnswers} from "../../utils/answers.ts";
-import {useEffect} from "react";
+import {getAnswersResults, getAnswersResultsToGraph, getLastRoll, getLastRollMainAnswers} from "../../utils/answers.ts";
+import {useEffect, useState} from "react";
 import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
 import DangerousIcon from '@mui/icons-material/Dangerous';
 import HourglassTopIcon from '@mui/icons-material/HourglassTop';
 import {getActivePlayer, getLastTurn} from "../../utils/game.ts";
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Legend,
+  Rectangle,
+  Tooltip,
+  XAxis,
+  YAxis
+} from "recharts";
+import Box from "@mui/material/Box";
+import {mobileCheck} from "../../utils/mobileCheck.ts";
 
 function Game() {
   const dispatch = useAppDispatch();
@@ -39,7 +48,12 @@ function Game() {
   const lastRollPlayerName = useAppSelector(selectPlayerName);
   const quizActive = useAppSelector(selectIsActive);
   const timerOn = useAppSelector(selectTimerOn);
+  const [activeTabNumber, setActiveTabNumber] = useState(0);
 
+  const handleChange = (event: any, newValue: number) => {
+    console.log(event)
+    setActiveTabNumber(newValue);
+  };
   useEffect(() => {
     if(game?.answersMode === 'true') {
       dispatch(show());
@@ -154,7 +168,7 @@ function Game() {
                                     <strong>Игра:</strong> {game.title} ({params.id})
                                     <button style={{marginLeft: 5}} onClick={onGetGameLink}>Ссылка на игру</button>
                                 </li>
-                                <li><strong>Смена:</strong> {getLastTurn(game)?.shift}</li>
+                                <li><strong>Смена:</strong> {getLastTurn(game)?.shift < 4 ? getLastTurn(game)?.shift : 'Игра окончена'}</li>
                                 <li><strong>Сейчас
                                     ходит:</strong> {getActivePlayer(game).name || getActivePlayer(game).username}</li>
 
@@ -214,22 +228,73 @@ function Game() {
                                   ?
 
                                   <div style={{marginTop: 20}}>
-                                    <table>
-                                      <tbody>
-                                      <tr>
-                                        <th>Игрок</th>
-                                        <th>Баллы</th>
-                                      </tr>
-                                      {
-                                        game.players.map((p: any) => {
-                                          return <tr>
-                                            <td>{p.name || p.username}</td>
-                                            <td>{getAnswersResults(game)[p.id]?.length}</td>
-                                          </tr>
-                                        })
-                                      }
-                                      </tbody>
-                                    </table>
+
+                                    <Box sx={{ borderBottom: 1, borderColor: 'divider', marginBottom: 2 }}>
+                                      <Tabs value={activeTabNumber} onChange={handleChange} aria-label="basic tabs example">
+                                        <Tab label="График" />
+                                        <Tab label="Таблица" />
+                                      </Tabs>
+                                    </Box>
+
+                                    <div
+                                      role="tabpanel"
+                                      hidden={activeTabNumber !== 0}
+                                      id={`simple-tabpanel-${0}`}
+                                      aria-labelledby={`simple-tab-${0}`}
+                                    >
+                                      {activeTabNumber === 0 && (
+                                        <Box sx={{ p: 3 }}>
+                                          <BarChart
+                                            width={mobileCheck() ? 320 : 500}
+                                            height={300}
+                                            data={getAnswersResultsToGraph(game)}
+                                            margin={{
+                                              top: 5,
+                                              right: 30,
+                                              left: 0,
+                                              bottom: 5,
+                                            }}
+                                          >
+                                            <CartesianGrid strokeDasharray="3 3" />
+                                            <XAxis dataKey="name" />
+                                            <YAxis />
+                                            <Tooltip />
+                                            <Legend />
+                                            <Bar dataKey="Баллы" fill="#c50000" activeBar={<Rectangle fill="#920000" stroke="#920000" />} />
+                                            <Bar dataKey="Попытки" fill="#594d5b" activeBar={<Rectangle fill="#c5c6d0" stroke="gray" />} />
+                                          </BarChart>
+                                        </Box>
+                                      )}
+                                    </div>
+
+                                    <div
+                                      role="tabpanel"
+                                      hidden={activeTabNumber !== 1}
+                                      id={`simple-tabpanel-${1}`}
+                                      aria-labelledby={`simple-tab-${1}`}
+                                    >
+                                      {activeTabNumber === 1 && (
+                                        <Box sx={{ p: 3 }}>
+                                          <table>
+                                            <tbody>
+                                            <tr>
+                                              <th>Игрок</th>
+                                              <th>Баллы</th>
+                                            </tr>
+                                            {
+                                              game.players.map((p: any) => {
+                                                return <tr>
+                                                  <td>{p.name || p.username}</td>
+                                                  <td>{getAnswersResults(game)[p.id]?.length}</td>
+                                                </tr>
+                                              })
+                                            }
+                                            </tbody>
+                                          </table>
+                                        </Box>
+                                      )}
+                                    </div>
+
                                   </div>
 
                                   :
